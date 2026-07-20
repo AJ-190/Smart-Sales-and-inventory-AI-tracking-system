36a5a2fb9b02500bea79ed3ab172c45744624bf5
Addy Samuel <adysamuel68@gmail.com>
2026-07-20 21:19:02 +0000

    fixed the database url issue

--- src/database.py
+++ src/database.py
@@ -4,11 +4,6 @@ from src.config import get_settings
 
 
 DATABASE_URL = get_settings().DATABASE_URL
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
-elif DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
-
 Base = declarative_base()
 
 engine = create_async_engine(
